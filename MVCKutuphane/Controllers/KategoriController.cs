8317a9c0a348abdff3608334e100d0455364c916
Sümeyye Nur Tetik <sumeyyeetetikk@gmail.com>
2026-05-09 12:01:06 +0300

    Add project files.

---
+++ MVCKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
+namespace MVCKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCategory(TBLKATEGORI p)
+        {
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
