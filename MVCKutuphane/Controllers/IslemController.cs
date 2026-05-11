c0f9dfcc07d40b526ddce85fde9f21ea99378361
Sümeyye Nur Tetik <sumeyyeetetikk@gmail.com>
2026-05-11 18:25:56 +0300

    Hareket Tablosu ve İşlemler

---
+++ MVCKutuphane/Controllers/IslemController.cs
@@ -0,0 +1,19 @@
+﻿using MVCKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCKutuphane.Controllers
+{
+    public class IslemController : Controller
+    {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHAREKET.Where(x => x.ISLEMDURUM == true).ToList();
+            return View(degerler);
+        }
+    }
+}
--- MVCKutuphane/Controllers/OduncController.cs
+++ MVCKutuphane/Controllers/OduncController.cs
@@ -1,8 +1,5 @@
 ﻿using MVCKutuphane.Models.Entity;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVCKutuphane.Controllers
@@ -10,21 +7,52 @@ namespace MVCKutuphane.Controllers
     public class OduncController : Controller
     {
         DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
-        public ActionResult Index()   
-        { 
-            var degerler = db.TBLHAREKET.ToList();
+
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHAREKET.Where(x=> x.ISLEMDURUM == false).ToList();
             return View(degerler);
         }
+
         [HttpGet]
         public ActionResult OduncVer()
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult OduncVer(TBLHAREKET p)
         {
             db.TBLHAREKET.Add(p);
+
+            var kitap = db.TBLKITAP.Find(p.KITAP);
+            kitap.DURUM = false;
+
             db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult OduncIade(int id)
+        {
+            var odn = db.TBLHAREKET.Find(id);
+            return View("OduncIade", odn);
+        }
+
+        [HttpPost]
+        public ActionResult OduncIade(TBLHAREKET p)
+        {
+            var hrk = db.TBLHAREKET.Find(p.ID);
+
+            hrk.UYEGETIRTARIH = p.UYEGETIRTARIH;
+            hrk.ISLEMDURUM = true;
+
+            var kitap = db.TBLKITAP.Find(hrk.KITAP);
+            kitap.DURUM = true;
+
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
     }
