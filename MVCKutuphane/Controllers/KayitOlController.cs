ae15afd9e6216e38dd2acd3cd5064b3a551d8b41
Sümeyye Nur Tetik <sumeyyeetetikk@gmail.com>
2026-05-27 15:01:56 +0300

    Register/login/ÖğrenciPaneli

---
+++ MVCKutuphane/Controllers/KayitOlController.cs
@@ -0,0 +1,32 @@
+﻿using MVCKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCKutuphane.Controllers
+{
+    public class KayitOlController : Controller
+    {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kayit(TBLUYELER p)
+        {
+
+            p.FOTOGRAF = "https://images.placeholder.com/150";
+
+            if (!ModelState.IsValid)
+            {
+                return View("Kayit");
+            }
+            db.TBLUYELER.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("GirisYap", "Login");
+        }
+    }
+}
